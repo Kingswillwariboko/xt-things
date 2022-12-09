00b660f9d0c08b449539019490b72d92799da6c0
Kingswill Wariboko <kingswillwariboko@gmail.com>
2022-12-09 14:11:28 +0100

    another change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react"
+
+import { VRButton, Interactive, ARButton, XR, Controllers, Hands } from '@react-three/xr'
+import { Canvas } from '@react-three/fiber'
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ARButton />
+      <Canvas>
+        <XR>
+          <Controllers />
+          <Hands />
+          <Interactive onSelect={() => console.log('clicked!')} onHover={() => setIsHovered(true)} onBlur={() => setIsHovered(false)}></Interactive>
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color="blue" />
+          </mesh>
+          <Interactive />
+        </XR>
+      </Canvas>
+    </>
+  )
 }
+
 
 export default App;
